f168edff7b33874a4c60baf9a8c19214f2dc933a
RmataM7 <rmuadthong@gmail.com>
2024-03-01 00:25:42 -0800

    Compare baselight w/ xytech
    Compare successfully
    bug: repeated new_location output under xytech_check for loop

--- Test/Sample_main.py
+++ Test/Sample_main.py
@@ -5,10 +5,9 @@ Xytech_file_path = "Test/Sample_Xytech.txt"
 xytech_folders = []
 
 try:
-    with open(Xytech_file_path, 'r') as read_xytech_file:  
-        # Iterate over each line in the file
+    with open(Xytech_file_path, 'r') as read_xytech_file:
+        # Filter out only line that contains "/" append in xytech_folders list
         for line in read_xytech_file:
-            # Check if the line contains "/"
             if "/" in line:
                 # If "/" is found, append the line to xytech_folders list
                 xytech_folders.append(line)
@@ -20,6 +19,7 @@ except FileNotFoundError:
 except PermissionError:
     print("Permission denied.")
 
+# print(xytech_folders)
 #import Baselight_export.txt
 Baselight_file_path = "Test/Sample_Baselight_export.txt"
 try:
@@ -27,11 +27,18 @@ try:
         for line in baselight_file:
             # Split baselight txt into list w/ whitespace as delimeter
             # Able to: seperate file path and frame 
-            line_split = line.split() 
+            line_split = line.split(" ") 
             # Pop 1st element in list which is always file path
             line_pop = line_split.pop(0)
-
+            line_replace = line_pop.replace("/baselightfilesystem1","")
+            new_location = ""
+            for xytech_check in xytech_folders:
+                if line_replace in xytech_check:
+                    new_location = xytech_check.strip()
+                    print(new_location)
 except FileNotFoundError:
     print("Baselight_export.txt file not found.")
 except PermissionError:
     print("Permission denied.")
+
+# print(new_location)
--- Test/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-for folder in xytech_folders:
-    #     print(folder.strip())
